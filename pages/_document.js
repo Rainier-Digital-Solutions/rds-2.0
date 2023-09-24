db95538e9f4cb02cb774e6288ba43defa984c2ec
Harper Foley <harper.e.foley@gmail.com>
2023-09-23 21:35:00 -0700

    Moves GTM scripts into appropriate spots

--- pages/_document.js
+++ pages/_document.js
@@ -30,14 +30,14 @@ const Document = () => {
         />
       </Head>
       <body>
-        <Main />
-        {/* <TwSizeIndicator /> */}
-        <NextScript />
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}" height="0" width="0" style="display: none; visibility: hidden;" />`,
           }}
         />
+        <Main />
+        {/* <TwSizeIndicator /> */}
+        <NextScript />
       </body>
     </Html>
   );
